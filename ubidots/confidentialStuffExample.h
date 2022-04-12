ba4b3439cf9d3598e1b20065a7040f8484a87109
Daniel Moraes <daniel.brugnaro.moraes@gmail.com>
2022-04-12 02:25:32 -0300

    [feature] - confidentialStuffExample.h addedd

---
+++ ubidots/confidentialStuffExample.h
@@ -0,0 +1,9 @@
+//To connect to Wi-Fi network:
+extern const char * WIFI_SSID = ""; // WiFi network name
+extern const char * WIFI_PASSWORD = ""; // WiFi network pwd
+extern const char * UBIDOTS_TOKEN = ""; // Ubidots personal token
+ 
+//To subscribe in the topic:
+extern const char* MQTTCLIENTNAME ""; // Specific topic generated in the script. Coulb be anything without special characters
+extern const char* SUB_DEVICE_LABEL = ""; // Device With the variable to be readen
+extern const char* SUB_VARIABLE_LABEL = ""; // The name you gave in the Ubidots portal
